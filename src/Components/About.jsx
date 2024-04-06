2f0422ba59991cb671c80ae708dcda709fbc86a6
DBAequilibrado <166240750+DBAequilibrado@users.noreply.github.com>
2024-04-06 17:18:19 -0300

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,18 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a data analyst who have the dream to become a DBA in the future";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Data Analysis",
+  "Database Administration",
+  "Cloud",
+  "ETL",
+  "Programming",
   "Graphic design",
 ];
 
